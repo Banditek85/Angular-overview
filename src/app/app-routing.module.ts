a2febee6751c972fe227048be45647c905be4004
Mitja Sušec <mitja.susec@codingmonkeys.net>
2019-08-10 12:51:40 +0200

    initialize app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { ObservablesComponent } from './observables/observables.component';
+import { RoutingComponent } from './routing/routing.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home", component: HomeComponent},
+  {path:"services", component: ServicesComponent},
+  {path:"observables", component: ObservablesComponent},
+  {path:"routing", component: RoutingComponent},
+  {path: "", redirectTo: "/home", pathMatch: "full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { ObservablesComponent } from './observables/observables.component';
+import { RoutingComponent } from './routing/routing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ServicesComponent,
+    ObservablesComponent,
+    RoutingComponent
   ],
   imports: [
     BrowserModule,
